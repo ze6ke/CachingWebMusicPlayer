c5a1278d9eb2838d45ce54db4a81c5e51c4b2146
Zeke Wander <git@ze6ke.com>
2017-08-30 05:15:52 +0000

    project management, identifying outstanding tasks

--- integration_tests/index.js
+++ integration_tests/index.js
@@ -1,15 +1,24 @@
-require('chromedriver');
+//initialize project
+//integrate with mocha (or something)
+//confirm the app loads
+//change song
+//get used space
+//clear store and confirm used space
+//filter songs
+//pause/restart music
+//
+require('chromedriver')
 
 
-var webdriver = require('selenium-webdriver');
-By = webdriver.By
-until = webdriver.until
-var driver = new webdriver.Builder()
+const webdriver = require('selenium-webdriver')
+const By = webdriver.By
+const until = webdriver.until
+const driver = new webdriver.Builder()
   .forBrowser('chrome')
-  .build();
+  .build()
 
-driver.get('http://www.google.com/ncr');
-driver.findElement(By.name('q')).sendKeys('webdriver');
-driver.findElement(By.css('input[value="Google Search"]')).click();
-driver.wait(until.titleIs('webdriver - Google Search'), 1000);
-driver.quit();
+driver.get('http://www.google.com/ncr')
+driver.findElement(By.name('q')).sendKeys('webdriver')
+driver.findElement(By.css('input[value="Google Search"]')).click()
+driver.wait(until.titleIs('webdriver - Google Search'), 1000)
+driver.quit()
